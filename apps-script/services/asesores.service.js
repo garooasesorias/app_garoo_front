0021486f8caad0e880283a928987aa903f9db91d
Robin Torres <robinhood9607@gmail.com>
2023-08-18 10:16:14 -0600

    million

--- apps-script/services/asesores.service.js
+++ apps-script/services/asesores.service.js
@@ -1,26 +1,26 @@
 function insertAsesor(document) {
-    const result = new MongoDBLib("asesores").insertDocument("insertOne",document);
-    return result;
-  }
-  
-  function getAsesores() {
-    const collectionName = "asesores"; // Replace with your collection name
-    const mongoDB = new MongoDBLib(collectionName);
-  
-    const query = {}; // Your query conditions
-    const order = { _id: 1 }; // Your sort order
-    const limit = 10; // Limit the number of documents
-  
-    const documents = mongoDB.getDocumentsWithSkillsAndEspecialidades(
-      "aggregate",
-      query,
-      order,
-      limit
-    );
-    console.log("Documents with related activities:", documents);
-    return documents;
-  }
-  
-  function getPlanById() {}
-  
-  function updatePlanById() {}
+  const result = new MongoDBLib("asesores").insertDocument("insertOne",document);
+  return result;
+}
+
+function getAsesores() {
+  const collectionName = "asesores"; // Replace with your collection name
+  const mongoDB = new MongoDBLib(collectionName);
+
+  const query = {}; // Your query conditions
+  const order = { _id: 1 }; // Your sort order
+  const limit = 10; // Limit the number of documents
+
+  const documents = mongoDB.getDocumentsWithSkillsAndEspecialidades(
+    "aggregate",
+    query,
+    order,
+    limit
+  );
+  console.log("Documents with related activities:", documents);
+  return documents;
+}
+
+function getPlanById() {}
+
+function updatePlanById() {}
